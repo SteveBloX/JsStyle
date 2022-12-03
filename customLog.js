a4541f7ac529943cb4a0f0ad9cc3119a1dfaab50
SteveBloX <steve.block@protonmail.com>
2022-12-03 12:20:48 +0100

    Add files

---
+++ customLog.js
@@ -0,0 +1,46 @@
+
+function customLog(text,
+    params = {
+
+      color: 'black',
+      size: '12'
+
+        }
+)
+{
+
+
+  console.log('%c' + text, 'color: ' + params.color + "; font-size: " + params.size + "px;");
+
+
+}
+
+function badge(
+    badgeText,
+    text,
+    badgeParams =
+        {
+        color: 'black',
+        size: '12',
+        borderColor: 'black',
+        backgroundColor: 'orange'
+        }
+)
+{
+
+  console.log('%c' + badgeText,
+      'border: 2px solid ' + badgeParams.borderColor +
+      ';color: ' + badgeParams.color +
+      "; font-size: " + badgeParams.size +
+      "px; background-color: " + badgeParams.backgroundColor +
+      ";border-radius:50px;padding-left:4px;padding-right:4px;padding-top:1px;padding-bottom:1px;",
+      text)
+
+}
+
+customLog('Hello World', {
+  color: 'red',
+  size: "16"
+});
+console.log('Hello World');
+badge('JsStyle', 'Hello World')
