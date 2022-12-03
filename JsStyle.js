fa9842c44a1f275eecd2c08cecded3cff8dea790
SteveBloX <steve.block@protonmail.com>
2022-12-03 16:29:25 +0100

    added border radius parameter to badge

--- JsStyle.js
+++ JsStyle.js
@@ -1,39 +1,24 @@
 
-function log(text,
-                 params = {
-
-      color: 'black',
-      size: '12'
-
-        }
-)
-{
-
-
-  console.log('%c' + text, 'color: ' + params.color + "; font-size: " + params.size + "px;");
-
-
+function log(text, params = {
+    color: 'black',
+    size: '12'
+}) {
+    console.log('%c' + text, 'color: ' + params.color + "; font-size: " + params.size + "px;");
 }
 
-function badge(
-    badgeText,
-    text,
-    badgeParams =
-        {
-        color: 'black',
-        size: '12',
-        borderColor: 'black',
-        backgroundColor: 'orange'
-        }
-)
-{
-
-  console.log('%c' + badgeText,
-      'border: 2px solid ' + badgeParams.borderColor +
-      ';color: ' + badgeParams.color +
-      "; font-size: " + badgeParams.size +
-      "px; background-color: " + badgeParams.backgroundColor +
-      ";border-radius:50px;padding-left:4px;padding-right:4px;padding-top:1px;padding-bottom:1px;",
-      text)
-
+function badge(badgeText, text, badgeParams = {
+    color: 'black',
+    size: '12',
+    borderColor: 'black',
+    borderRadius: '50',
+    backgroundColor: 'orange'
+}) {
+    console.log('%c' + badgeText,
+        'border: 2px solid ' + badgeParams.borderColor +
+        ';color: ' + badgeParams.color +
+        "; font-size: " + badgeParams.size +
+        "px; background-color: " + badgeParams.backgroundColor +
+        ";border-radius:" + badgeParams.borderRadius +
+        "px;padding-left:4px;padding-right:4px;padding-top:1px;padding-bottom:1px;",
+        text)
 }
